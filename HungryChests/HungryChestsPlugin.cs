a836e217666f111a8b7f8da1d5671b8a358be156
CubeSuite <cubesuite@outlook.com>
2023-08-01 12:17:48 +0100

    Initial commit

---
+++ HungryChests/HungryChestsPlugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using HungryChests.Patches;
+using UnityEngine;
+
+namespace HungryChests
+{
+    // TODO Review this file and update to your own requirements.
+
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class HungryChestsPlugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.equinox.HungryChests";
+        private const string PluginName = "HungryChests";
+        private const string VersionString = "1.0.0";
+
+        private const string VoidableItemsKey = "VoidableItems";
+        public static ConfigEntry<string> VoidableItems;
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private void Awake() {
+            VoidableItems = Config.Bind("General", VoidableItemsKey, "Limestone,Plantmatter,Kindlevine Extract,Shiverthorn Extract", new ConfigDescription("Items that will be auto-voided from the last slot in a chest"));
+
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.PatchAll();
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+            Log = Logger;
+
+            Harmony.PatchAll(typeof(InserterPatch));
+        }
+    }
+}
---
+++ HungryChests/Patches/InserterPatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace HungryChests.Patches
+{
+    public class InserterPatch
+    {
+        [HarmonyPatch(typeof(InserterInstance), "Give")]
+        [HarmonyPrefix]
+        static void deleteLastChestSlot(InserterInstance __instance) {
+            if(__instance.giveResourceContainer.typeIndex == MachineTypeEnum.Chest) {
+                Inventory inventory = __instance.giveResourceContainer.GetInventory(0);
+                if (!inventory.myStacks[55].isEmpty && getVoidableItems().Contains(inventory.myStacks[55].info.displayName)) {
+                    inventory.RemoveResourcesFromSlot(inventory.numSlots - 1, inventory.myStacks[55].count);
+                }
+            }
+        }
+
+        private static List<string> getVoidableItems() {
+            return HungryChestsPlugin.VoidableItems.Value.Split(',').ToList();
+        }
+    }
+}
